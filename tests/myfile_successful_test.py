69405cbb1d0c5947101820b3f01a4dd28329a31e
Bhushan0504 <79035033+Bhushan0504@users.noreply.github.com>
2024-08-25 19:44:03 +0000

    Added test engine and test files

---
+++ tests/myfile_successful_test.py
@@ -0,0 +1,10 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from myfile import MyFile
+def test_add():
+    mf = MyFile()
+    a = mf.add_numbers(2,3)
+    assert a == 5
